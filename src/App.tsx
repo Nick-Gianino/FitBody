f51624d30d71c308c84d9ee17a6561692bd510d7
Nick Gianino <nagianino@loyola.edu>
2023-08-07 21:02:55 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC82UB6VC8I67ZkcifecBG38axkhFA5J3w",
+    authDomain: "fitbodyandwellness.firebaseapp.com",
+    projectId: "fitbodyandwellness",
+    storageBucket: "fitbodyandwellness.appspot.com",
+    messagingSenderId: "1013767472704",
+    appId: "1:1013767472704:web:999bbc8b3aaf92c377c1a5",
+    measurementId: "G-00QW7BVWXC"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>penis</p>
     </div>
   );
 }
